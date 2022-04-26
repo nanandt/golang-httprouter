a5faec7db004043d4de35661837ac210272ea58f
nanandt <rizkyfatih007@gmail.com>
2022-04-26 22:20:05 +0700

    Params

---
+++ serve_file_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRouter(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+		fmt.Fprint(writer, "Hello GET")
+	})
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello GET", string(body))
+}
